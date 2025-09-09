1c03cb947eb05ddab1818d351760bb11fb9ae258
Sachin-8125 <devinewolf7@gmail.com>
2025-09-09 17:28:38 +0530

    Frontend mod files updated

---
+++ todo-app/frontend/src/components/mod.rs
@@ -0,0 +1,5 @@
+mod auth;
+mod todolist;
+
+pub use auth::Auth;
+pub use todolist::TodoList;
--- todo-app/frontend/src/main.rs
+++ todo-app/frontend/src/main.rs
@@ -16,13 +16,37 @@ fn app() -> Html{
     let auth_service = use_memo((), |_| AuthService::new());
     html! {
         <BrowserRouter>
-            
+            <div class="min-h-screen bg-gray-100">
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        <h1 class="text-3xl font-bold text-gray-900"> {"Todo App"}</h1>
+                    </div>
+                </header>
+                <main>
+                    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                        <Switch<Route> render={move |route| {
+                            let auth_service = auth_service.clone();
+                            match route {
+                                Route::Home => {
+                                    if auth_service.is_logged_in(){
+                                        html!{<TodoList/>}
+                                    }else{
+                                        html!{<Auth/>}
+                                    }
+                                }
+                                Route::Login => html! {<Auth/>},
+                            }
+                        }}/>
+                    </div>
+                </main>
+
+            </div>
 
         </BrowserRouter>
 
     }
 
-}
+    }
 
 
 
